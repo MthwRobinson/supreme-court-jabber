1e6358f346b830c9a83c7a843d9e67e6944a8e39
Matt Robinson <mthw.wm.robinson@gmail.com>
2021-10-19 13:27:01 -0400

    added function for printing urls

--- data/http/http.go
+++ data/http/http.go
@@ -2,28 +2,49 @@
 package fetch_arguments
 
 import (
-  "log"
-  "net/http"
-  "net/http/httputil"
+	"fmt"
+	"golang.org/x/net/html"
+	"log"
+	"net/http"
+	"net/http/httputil"
 )
-
 
 const supremeCourtURL = "https://www.supremecourt.gov/oral_arguments/argument_transcript/2021"
 
-
 func GetOralArguments() {
-  client := &http.Client{}
+	client := &http.Client{}
 
-  response, err := client.Get(supremeCourtURL)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer response.Body.Close()
+	response, err := client.Get(supremeCourtURL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
 
-  body, err := httputil.DumpResponse(response, true)
-  if err != nil {
-    log.Fatal(err)
-  }
+	body, err := httputil.DumpResponse(response, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(string(body))
 
-  log.Println(string(body))
+	z := html.NewTokenizer(response.Body)
+	for {
+		tt := z.Next()
+		switch tt {
+		case html.ErrorToken:
+			fmt.Println("Done!")
+			return
+		case html.StartTagToken, html.EndTagToken:
+			token := z.Token()
+			if "a" == token.Data {
+				for _, attr := range token.Attr {
+					if attr.Key == "href" {
+						fmt.Println(attr.Val)
+					}
+				}
+			}
+
+		}
+
+	}
+
 }
